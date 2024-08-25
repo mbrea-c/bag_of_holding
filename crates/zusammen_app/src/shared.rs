0601fd10bf81a574f25e1d8927862e8950493f5d
Manuel Brea <m.brea.carreras@gmail.com>
2024-08-25 20:09:41 +0100

    fixito

--- crates/zusammen_app/src/shared.rs
+++ crates/zusammen_app/src/shared.rs
@@ -3,12 +3,17 @@ use std::sync::Arc;
 use bevy::prelude::*;
 use zusammen_plugin::ZusammenPlugin;
 
+use crate::protocol::ProtocolPlugin;
+
 pub struct SharedPlugin {
     pub zusammen: Arc<dyn ZusammenPlugin + Send + Sync + 'static>,
 }
 
 impl Plugin for SharedPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins(ProtocolPlugin {
+            zusammen: self.zusammen.clone(),
+        });
         self.zusammen.add_shared(app);
     }
 }
