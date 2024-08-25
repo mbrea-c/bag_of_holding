b1e9d3062ae2149aba800fb53ab97840743a822b
Manuel Brea <m.brea.carreras@gmail.com>
2024-08-25 18:08:14 +0100

    fix visibility

--- crates/zusammen_app/src/lib.rs
+++ crates/zusammen_app/src/lib.rs
@@ -5,4 +5,5 @@ mod protocol;
 mod server;
 mod shared;
 
+pub use apps::{ClientParams, ClientTransportParams, NetParameters, ServerParams, SharedParams};
 pub use core::{run_multiplayer_app, ClientZusammenAppManager, ZusammenAppConfig, ZusammenAppMode};
